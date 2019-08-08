8383f59b4510d0bd63d8a617b337496a1c7b627d
Eduardo G <eguerrerom@ucenfotec.ac.cr>
2019-08-08 15:23:41 -0600

    added load more characters functionality

--- src/containers/chracters/character.js
+++ src/containers/chracters/character.js
@@ -1,40 +1,62 @@
 import React,{ Component } from 'react';
 import Axios from 'axios';
 import CharacterPost from '../../components/character-post/CharacterPost';
+import './character.css';
 
 class Character extends Component {
-  state = {
-    characters: []
+  constructor(props){
+    super(props);
+
+    this.state = {
+      characters: [],
+      visible: 6,
+    }
+
+    this.loadMore = this.loadMore.bind(this);
+  }
+
+  loadMore = () => {
+    this.setState((prev) => {
+      console.log(prev);
+      return {visible: prev.visible + 6}
+    })
   }
 
   componentDidMount() {
 
     Axios.get('https://breakingbadapi.com/api/characters')
       .then(result => {
-          console.log(result.data);
           this.setState({characters: result.data});
       })
   }
 
   render () {
-    const characters = this.state.characters.map(character => {
-      return <CharacterPost key={character.char_id}
+    const characters = this.state.characters.slice(0,this.state.visible).map(character => {
+      return (<CharacterPost key={character.char_id}
         img={character.img}
         name={character.name}
         occupation={character.occupation}
         status={character.status}
         birthday={character.birthday}
-        portrayed={character.portrayed}/>
-    })
+       portrayed={character.portrayed}/>
+      )
+    });
     return (
       <div className="container-fluid">
         <div className="row">
           {characters}
         </div>
+        {this.state.visible < this.state.characters.length &&
+        <button onClick={this.loadMore} type="button" className="load-more">Load more</button>}
       </div>
-    )
-  }
 
+    );
+  }
 }
 
-export default Character;
+
+  export default Character;
+
+
+
+
